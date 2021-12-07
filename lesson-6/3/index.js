b491b511573e33729a36e945caf45b42cadbd0eb
AndreiYar <yarovoi.a@do-it.co>
2021-12-07 18:09:56 +0200

    Update task 3.

--- lesson-6/3/index.js
+++ lesson-6/3/index.js
@@ -1,21 +1,20 @@
 const { pipeline } = require('stream');
 const fs = require('fs');
-const zlib = require('zlib');
 const Json2csv = require('./src/Json2csv');
+const Archiver = require('./src/Archiver');
 
 const usersReadableStream = fs.createReadStream('./src/data/users.json');
-const gzip = zlib.createGzip();
-const unArch = zlib.createGunzip(); // деархиватор
 const usersWritableStream = fs.createWriteStream('./src/data/users.csv');
 const fields = ['postId', 'name', 'body'];
 
 const json2csv = new Json2csv(fields);
+const archiver = new Archiver();
 
 pipeline(
   usersReadableStream,
   json2csv,
-  gzip,
-  unArch,
+  archiver.archive(),
+  archiver.unarchive(),
   usersWritableStream,
   (err) => {
     err ? console.log('Pipeline failed.', err) : console.log('Pipeline succeeded.');
