f478f173a951dee6b133ac14ae8564b7f8d0dc12
AndreiYar <yarovoi.a@do-it.co>
2022-02-01 19:49:05 +0200

    Finish task 2.

--- lesson-8/task-2/index.test.js
+++ lesson-8/task-2/index.test.js
@@ -5,21 +5,32 @@ jest.mock('events');
 describe('Test Bank:', () => {
   test('Register new customer', () => {
     const bank = new Bank();
-    const customerId = bank.register({ name: 'test' })
+    const customerId = bank.register({ name: 'test', balance: 0 })
 
     expect(customerId).toBeGreaterThan(0);
   });
   test('Test duplication of customers', () => {
     const bank = new Bank();
-    bank.register({ name: 'test' })
-    bank.register({ name: 'test' })
+    const name = 'test';
+    const customer  = { name: name, balance: 0 };
+    bank.register(customer)
+    bank.register(customer)
 
-    expect(bank.emit).toHaveBeenCalledWith("error", new Error(`duplicated customer for name: 'test'`));
+    expect(bank.emit).toHaveBeenCalledWith("error", new Error(`duplicated customer for name: '${name}'`));
   });
-  test('Test add customer', () => {
+  test('Test add event', () => {
     const bank = new Bank();
-    bank.emit('add', 1, -1)
+    const personId = 1;
+    const amount = 1;
+    bank.emit('add', personId, amount)
 
-    expect(bank.emit).toHaveBeenNthCalledWith(2, 'error', new Error('amount should be grater than 0'));
+    expect(bank.emit).toHaveBeenCalledWith('add', personId, amount);
+  });
+  test('Test enroll function with negative balance', () => {
+    const bank = new Bank();
+    const customerId = bank.register({ name: 'test', balance: 0 });
+    bank._enroll(customerId, -1);
+
+    expect(bank.emit).toHaveBeenCalledWith('error', new Error('amount should be grater than 0'));
   });
 });
